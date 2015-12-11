d3489da2eb7593828700516b0dd4da7cd165e934
masanorimasuda <makurisan41228@yahoo.co.jp>
2015-12-12 03:09:46 +0900

    アーカイブページ１ページ追加

--- fuel/fuel/app/classes/view/archive/chartnews.php
+++ fuel/fuel/app/classes/view/archive/chartnews.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * index/chartページ表示用ViewModel
+ * index/chart_newsページ表示用ViewModel
  *
  */
-class View_Index_Chart extends ViewModel {
+class View_Archive_Chartnews extends ViewModel {
 	public function view() {
 		$data = array();
 
@@ -12,37 +12,26 @@ class View_Index_Chart extends ViewModel {
 		krsort($data['img_dir_list']);
 
 		// サイドバー配列作成
+		$array_count = 0;
 		foreach($data['img_dir_list'] as $key_img => $value_img) {
 			$tmp_text = str_replace("/","",$key_img);
+			if($array_count == 0) {
+				// セットされた日にち
+				$set_date = ($this->set_data['date_str'] == "--") ? str_replace("_","-",$tmp_text) : $this->set_data['date_str'];
+			}
+
 			$tmp_array = explode("_",$tmp_text);
 
 			if(count($tmp_array) == 3) $data["date_list_array"][$tmp_array[0]][] = $tmp_text;
+			$array_count++;
 		}
 
-		// セットされた日にち
-		$data["set_date"] = $this->set_data['date_str'];
-		if($data["set_date"] == "--") {
-			if(date("N") == 1) {
-				//月曜日のとき
-				$data["set_date"] = date("Y-m-d", strtotime("-3 day"));
-				$data['yesterday'] = date("Y-m-d", strtotime("-3 day"));
-			}else {
-				if(date("H") < 6 ) {
-					$data["set_date"] = date("Y-m-d", strtotime("-1 day -6 hours"));
-					$data['yesterday'] = date("Y-m-d", strtotime("-1 day -6 hours"));
-				}else {
-					$data["set_date"] = date("Y-m-d", strtotime("-1 day"));
-					$data['yesterday'] = date("Y-m-d", strtotime("-1 day"));
-				}
-			}
-		}else {
-			$data['yesterday'] = $data["set_date"];
-		}
-		
-		$data['file_exist_flag'] = ( array_key_exists(str_replace("-","_",$data['yesterday']).'/' , $data['img_dir_list']) ) ? true : false;
-		
+		// 日付調整
+		$data['set_date'] = date("Y-m-d", strtotime("${set_date}"));
+		$data['yesterday'] = date("Y-m-d", strtotime("${set_date}"));
+
 		// 指定日の変動データ
-		$data["currency_datas"] = Model_Dailydatum::get_data_bydate($data["set_date"]);
+		$data["currency_datas"] = Model_Dailydatum::get_data_bydate($data["yesterday"]);
 		if (empty($data["currency_datas"])) {
 			array_push($data["currency_datas"],array('currency' => 'AUDUSD'));
 			array_push($data["currency_datas"],array('currency' => 'AUDJPY'));
@@ -53,17 +42,17 @@ class View_Index_Chart extends ViewModel {
 			array_push($data["currency_datas"],array('currency' => 'NZDUSD'));
 			array_push($data["currency_datas"],array('currency' => 'EURUSD'));
 		}
-
 		$data['setting_percent'] = 0.5;
+
 		// 指定日のnews一覧
 		$data['news'] = Model_News_Before::find('all', array(
 			'where' => array(
-				array('date', $this->set_data['date_str']),
+				array('date', $data['set_date']),
 			),
 		));
 
+
 		//テンプレートを変更する場合
-		$this->_view = View::forge('index/chart',$data);
+		$this->_view = View::forge('archive/chartnews',$data);
 	}
 }
-
